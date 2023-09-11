2952f8109e2c9cafdaf81168311933aff9f6c719
Graham2000 <wghiltonreads@gmail.com>
2023-09-11 00:17:47 -0400

    Retrieve list contents

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("test");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const getWordList = (src) => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield fetch(src);
+    return ((yield response.text()).split("\n"));
+});
+getWordList("../res/answer-list.txt").then(res => {
+    console.log(res);
+});
+getWordList("../res/guess-list.txt").then(res => {
+    console.log(res);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("test")
+//let answerList: string[] = [];
+//let guessList: string[] = [];
+
+const getWordList = async (src: string): Promise<string[]> => {
+    const response = await fetch(src);
+    return ((await response.text()).split("\n"));
+}
+
+getWordList("../res/answer-list.txt").then(res => {
+    console.log(res);
+});
+
+getWordList("../res/guess-list.txt").then(res => {
+    console.log(res);
+});
+
