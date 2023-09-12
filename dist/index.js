47f45bbbbead2a03bbfb689373ece815a13559d2
Graham2000 <wghiltonreads@gmail.com>
2023-09-11 22:08:58 -0400

    Auto focus active input

--- dist/index.js
+++ dist/index.js
@@ -8,13 +8,79 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+window.onload = () => {
+    let boxes = document.getElementsByClassName("box");
+    boxes[0].disabled = false;
+    boxes[0].focus();
+    let overlay = document.getElementById("overlay");
+    overlay.addEventListener("click", () => {
+        for (let i = 0; i < boxes.length; i++) {
+            if (boxes[i].disabled === false) {
+                boxes[i].focus();
+            }
+        }
+    });
+};
+const ANSWER_SRC = "../res/answer-list.txt";
+const GUESS_SRC = "../res/guess-list.txt";
 const getWordList = (src) => __awaiter(void 0, void 0, void 0, function* () {
-    const response = yield fetch(src);
-    return ((yield response.text()).split("\n"));
+    const list = yield fetch(src);
+    return ((yield list.text()).split("\n"));
 });
-getWordList("../res/answer-list.txt").then(res => {
-    console.log(res);
+getWordList(ANSWER_SRC).then(answerList => {
+    console.log(answerList);
 });
-getWordList("../res/guess-list.txt").then(res => {
-    console.log(res);
+getWordList(GUESS_SRC).then(guessList => {
+    console.log(guessList);
 });
+const moveToPrev = (input) => {
+    input.value = "";
+    input.previousElementSibling.disabled = false;
+    input.previousElementSibling.focus();
+    input.disabled = true;
+};
+const moveToNext = (input) => {
+    input.nextElementSibling.disabled = false;
+    input.nextElementSibling.focus();
+    input.disabled = true;
+};
+const newGuess = (currBox, nextRow) => {
+    if (nextRow) {
+        currBox.disabled = false;
+        currBox.focus();
+    }
+    else {
+        alert("You are out of guesses! The answer was: wod");
+    }
+};
+const isLetter = (c) => {
+    return c.toLowerCase() != c.toUpperCase();
+};
+const ENTER_KEY_CODE = 13;
+const BACKSPACE_KEY_CODE = 8;
+const handleKeyPress = (e) => {
+    let prevInput = e.target.previousElementSibling;
+    let nextInput = e.target.nextElementSibling;
+    let nextRow;
+    let currBox;
+    if (e.target.parentElement.nextElementSibling &&
+        e.target.parentElement.nextElementSibling.firstElementChild) {
+        nextRow = e.target.parentElement.nextElementSibling;
+        currBox = e.target.parentElement.nextElementSibling.firstElementChild;
+    }
+    setTimeout(() => {
+        if (e.keyCode === ENTER_KEY_CODE) {
+            e.target.disabled = true;
+            newGuess(currBox, nextRow);
+        }
+        else if (e.keyCode == BACKSPACE_KEY_CODE) {
+            prevInput && moveToPrev(e.target);
+        }
+        else if (isLetter(e.target.value)) {
+            nextInput && moveToNext(e.target);
+        }
+        else {
+            e.target.value = "";
+        }
+    });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,119 @@
 //let answerList: string[] = [];
 //let guessList: string[] = [];
 
-const getWordList = async (src: string): Promise<string[]> => {
-    const response = await fetch(src);
-    return ((await response.text()).split("\n"));
+// Init entry and add event listeners
+window.onload = () => {
+    let boxes: any = document.getElementsByClassName("box");
+
+    // Entry box
+    boxes[0].disabled = false;
+    boxes[0].focus();
+
+    let overlay: any = document.getElementById("overlay");
+
+    overlay.addEventListener("click", () => {
+        for(let i = 0; i < boxes.length; i++) {
+            if (boxes[i].disabled === false) {
+                boxes[i].focus();
+            }
+    
+        }
+    });
+
 }
 
-getWordList("../res/answer-list.txt").then(res => {
-    console.log(res);
+const ANSWER_SRC: string = "../res/answer-list.txt";
+const GUESS_SRC: string = "../res/guess-list.txt";
+
+const getWordList = async (src: string): Promise<string[]> => {
+    const list = await fetch(src);
+    return ((await list.text()).split("\n"));
+}
+
+getWordList(ANSWER_SRC).then(answerList => {
+    console.log(answerList);
 });
 
-getWordList("../res/guess-list.txt").then(res => {
-    console.log(res);
+getWordList(GUESS_SRC).then(guessList => {
+    console.log(guessList);
 });
 
+///////////////////////////////
+// Generate random word of day from answer list
+
+
+
+
+
+
+
+////////////////////////////////
+
+const moveToPrev = (input: any): void => {
+    input.value = "";
+    input.previousElementSibling.disabled = false;
+    input.previousElementSibling.focus();
+    input.disabled = true;
+}
+
+const moveToNext = (input: any): void => {
+    input.nextElementSibling.disabled = false;
+    input.nextElementSibling.focus();
+    input.disabled = true
+}
+
+// New row
+const newGuess = (currBox: any, nextRow: any): void => {
+    if (nextRow) {
+        currBox.disabled = false;
+        currBox.focus();
+    } else {
+        alert("You are out of guesses! The answer was: wod");
+    }
+}
+
+
+const isLetter = (c: string): boolean => {
+    return c.toLowerCase() != c.toUpperCase();
+}
+
+// Add event listener for enter key
+const ENTER_KEY_CODE: number = 13;
+const BACKSPACE_KEY_CODE: number = 8;
+
+const handleKeyPress = (e: any) => {
+    let prevInput: HTMLElement = e.target.previousElementSibling;
+    let nextInput: HTMLElement = e.target.nextElementSibling;
+
+    let nextRow: HTMLElement;
+    let currBox: HTMLElement;
+
+    if (e.target.parentElement.nextElementSibling && 
+        e.target.parentElement.nextElementSibling.firstElementChild) {
+        nextRow = e.target.parentElement.nextElementSibling;
+        currBox = e.target.parentElement.nextElementSibling.firstElementChild;
+    }
+
+    setTimeout(() => {
+        
+        if (e.keyCode === ENTER_KEY_CODE) {
+            // If last box in row
+                // Check if word is valid guess or answer
+
+            e.target.disabled = true;
+            newGuess(currBox, nextRow);
+            
+        } else if (e.keyCode == BACKSPACE_KEY_CODE) {
+
+            prevInput && moveToPrev(e.target);
+
+        } else if (isLetter(e.target.value)) {    
+
+            nextInput && moveToNext(e.target);
+
+        } else {
+            e.target.value = "";
+        }
+        
+    });
+}
